4f8d7a182ed21439c5b5198463ca4bad92b584f5
Clement Callejon <clement.callejon@tutanota.com>
2025-04-12 02:04:37 +0200

    Fresh start on VM

--- configuration.nix
+++ configuration.nix
@@ -5,165 +5,150 @@
 { config, pkgs, ... }:
 
 {
-	imports =
-		[ # Include the results of the hardware scan.
-		./hardware-configuration.nix
-		];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-# Bootloader.
-	boot.loader.grub = {
-		enable = true;
-		device = "/dev/sda";
-		useOSProber = true;
-	};
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
-# Setup keyfile
-	boot.initrd.secrets = {
-		"/boot/crypto_keyfile.bin" = null;
-	};
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-	boot.loader.grub.enableCryptodisk = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-	boot.initrd.luks.devices."luks-294daf6e-cb50-4686-93db-4b2496d8a054".keyFile = "/boot/crypto_keyfile.bin";
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-	networking.hostName = "nixos"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Enable network manager applet
+  programs.nm-applet.enable = true;
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
 
-# Enable networking
-		networking.networkmanager.enable = true;
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
-# Set your time zone.
-	time.timeZone = "Europe/Paris";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
 
-# Select internationalisation properties.
-	i18n.defaultLocale = "en_US.UTF-8";
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "fr_FR.UTF-8";
-		LC_IDENTIFICATION = "fr_FR.UTF-8";
-		LC_MEASUREMENT = "fr_FR.UTF-8";
-		LC_MONETARY = "fr_FR.UTF-8";
-		LC_NAME = "fr_FR.UTF-8";
-		LC_NUMERIC = "fr_FR.UTF-8";
-		LC_PAPER = "fr_FR.UTF-8";
-		LC_TELEPHONE = "fr_FR.UTF-8";
-		LC_TIME = "fr_FR.UTF-8";
-	};
+  # Enable the LXQT Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.lxqt.enable = true;
 
-# Enable flakes
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "azerty";
+  };
 
-# Nix generations garbage collection
-	nix.gc = {
-		automatic = true;
-		dates = "weekly";
-		options = "--delete-older-than 30d";
-	};
+  # Configure console keymap
+  console.keyMap = "fr";
 
-# Configure console keymap
-	console.keyMap = "fr";
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-# Window manager
-	programs.sway = { 
-		enable = true;
-	};
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-	programs.hyprland = {
-		enable = true;
-		xwayland.enable = true;
-		withUWSM = true;
-	};
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-	environment.sessionVariables = {
-# To fix invisible cursor
-		WLR_NO_HARDWARE_CURSORS = "1";
-# Hint Electron apps to use Wayland
-		NIXOS_OZONE_WL = "1";
-	};
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-	hardware = {
-# OpenGL
-		graphics.enable = true;
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.clement = {
+    isNormalUser = true;
+    description = "Clement";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
 
-# Most Wayland compositors need this
-		nvidia.modesetting.enable = true;
-	};
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.clement = {
-		isNormalUser = true;
-		description = "Clement";
-		extraGroups = [ "networkmanager" "wheel" ];
-		packages = with pkgs; [];
-	};
+  # Versioning
+  programs.git.enable = true;
 
-# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
+  # Text editor
+  programs.neovim.enable = true;
 
-# List packages installed in system profile. 
-# To search, run:
-# $ nix search wget
-	environment.systemPackages = with pkgs; [
-# Compilation
-		gnumake
-# File download
-		wget
-# Text editor
-			vim
-			neovim
+  # Shell
+  programs.zsh.enable = true;
 
-# Versioning
-			git
+  # Web Browser
+  programs.firefox.enable = true;
 
-# Home manager
-			home-manager
-# Desktop utilities
-			grim # screenshot functionality
-			slurp # screenshot functionality
-			wl-clipboard # Copy paste from stdin/stdout
-			mako # notification system
-# Browser
-			brave
-			firefox
-# Shell
-			zsh
-# Home manager
-			home-manager
-			];
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
 
-# Enable the gnome-keyring secret vault.
-# Will be exposed through DBus to programs willing to store secrets.
-	services.gnome.gnome-keyring.enable = true;
+  # Compilation
+  	gnumake
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
+  # File download
+	wget
 
-# List services that you want to enable:
+  # Home manager
+  home-manager
 
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
+  ];
 
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "24.11"; # Did you read the comment?
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
