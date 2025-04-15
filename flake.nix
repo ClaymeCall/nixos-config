7e0c2997a2fccbbf5035ccd8c66984f150e44ed2
Clement Callejon <clement.callejon@tutanota.com>
2025-04-15 13:17:29 +0200

    Added basic flake and host configuration

--- flake.nix
+++ flake.nix
@@ -4,19 +4,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        # inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      # Configuration for the VM
+      vm = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./configuration.nix
+	  ./vm/hardware-configuration.nix
+	  inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      # Configuration for Idealis
+      # idealis = nixpkgs.lib.nixosSystem {
+      #   specialArgs = {inherit inputs;};
+      #   modules = [
+      #     ./configuration.nix
+      #     ./idealis/hardware-configuration.nix
+      #     inputs.home-manager.nixosModules.default
+      #   ];
+      # };
     };
   };
 }
