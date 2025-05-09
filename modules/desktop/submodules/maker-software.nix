c93dad17f50267faf4ad738e0c47e91d86c4b68a
Clement Callejon <clement.callejon@tutanota.com>
2025-05-09 02:57:32 +0200

    Added toggleable parts to maker software module

--- modules/desktop/submodules/maker-software.nix
+++ modules/desktop/submodules/maker-software.nix
@@ -2,25 +2,46 @@
   lib,
   config,
   ...
-}: {
-  ############################
-  # Maker Software Submodule #
-  ############################
+}:
+############################
+# Maker Software Submodule #
+############################
+with lib; let
+  cfg = config.maker-software;
+in {
+  options.maker-software = {
+    enable =
+      mkEnableOption "Enables various maker software";
+
+    CAD = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable CAD software (e.g. FreeCAD)";
+    };
+
+    slicer = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable Slicer software (e.g. Orca Slicer)";
+    };
+  };
+
+  config = {
+    # Provide defaults when `enable = true` and user doesn't override
+    maker-software = mkIf cfg.enable {
+      CAD = mkDefault true;
+      slicer = mkDefault true;
+    };
+
+    # Map purpose-driven toggles to actual programs
+    freecad.enable = cfg.CAD;
+    openscad.enable = cfg.CAD;
+    orca-slicer.enable = cfg.slicer;
+  };
 
   imports = [
-    ../programs/orca-slicer.nix # Slicer for 3D printer gcode
-    ../programs/freecad.nix # GUI CAD software
-    ../programs/openscad.nix # Code CAD software
+    ../programs/orca-slicer.nix
+    ../programs/freecad.nix
+    ../programs/openscad.nix
   ];
-
-  options = {
-    maker-software.enable =
-      lib.mkEnableOption "Enables various maker software";
-  };
-
-  config = lib.mkIf config.maker-software.enable {
-    orca-slicer.enable = true;
-    freecad.enable = true;
-    openscad.enable = true;
-  };
 }
